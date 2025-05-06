0b510390b1a8239fe3e43f996ee25baae9d8ae14
Abir-dev <abirlalbanerjee44@gmail.com>
2025-05-07 00:26:50 +0530

    Did The Page Layout

--- app/routes/root/page-layout.tsx
+++ app/routes/root/page-layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {logoutUser} from "~/appwrite/auth";
-import {useNavigate} from "react-router";
+import {Link, useNavigate} from "react-router";
 
 const PageLayout = () => {
     const navigate = useNavigate();
@@ -9,18 +9,32 @@ const PageLayout = () => {
         navigate('/sign-in');
     }
     return (
-        <div>
-            <button
-                onClick={handleLogout}
-                className="cursor-pointer"
-            >
-                <img src="/assets/icons/logout.svg" alt="logout" className="size-6" />
-            </button>
+        <main className="travel-hero min-h-screen ">
 
-            <button onClick={() => {navigate('/dashboard')}}>
-                Dashboard
-            </button>
-        </div>
+           <section className=" flex px-30 py-4 min-h-screen">
+                   <Link to=''>
+                       <img
+                           src="/assets/icons/logo.svg"
+                           alt='Logo'
+                           className='size-[30px]'
+                       />
+                   </Link>
+                   <h1 className='p-28-semibold text-dark-100 mt-1 mr-200 ml-2'>Tourvisto</h1>
+
+
+               <button onClick={() => {navigate('/dashboard')}}
+               className="cursor-pointer mb-200 text-white mt-1/2">
+                   Admin Panel
+               </button>
+
+               <button
+                   onClick={handleLogout}
+                   className="cursor-pointer"
+               >
+                   <img src="/assets/icons/logout.svg" alt="logout" className="size-6 mb-200  ml-2" />
+               </button>
+           </section>
+        </main>
     )
 }
 export default PageLayout
